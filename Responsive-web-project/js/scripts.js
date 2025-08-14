398f3487be74727ec134810a85f66e3b356b7a94
GenoKo <153858382+GenoKo@users.noreply.github.com>
2025-08-14 12:50:03 +0700

    Add files via upload

---
+++ Responsive-web-project/js/scripts.js
@@ -0,0 +1,81 @@
+/*!
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
+
+
+$(window).bind('scroll',function(e){
+    colorScroll();
+});
+
+function colorScroll(){
+    var scrolled = $(window).scrollTop(); 
+    
+    var baseNum = scrolled % 256;
+
+    function clamp(val) {
+        return Math.max(0, Math.min(255, val));
+    }
+
+    var r = clamp(baseNum * 3);
+    var g = clamp(baseNum * 5);
+    var b = clamp(baseNum * 2);
+
+
+    $('.thebigthing').css('color', `rgb(${r}, ${g}, ${b})`);
+
+    console.log(scrolled);
+}
+
+
